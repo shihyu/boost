7841f19038660b2abae5e14d02c7d7c94a97a4cf
jason_yao <jason_yao@htc.com>
2013-10-17 22:19:33 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,30 @@
+
+
+
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <iostream>
+using namespace std;
+
+class D: public boost::enable_shared_from_this<D>
+{
+public:
+    D()
+    {
+        cout<<"D::D()"<<endl;
+    }
+    
+    void func()
+    {
+        cout<<"D::func()"<<endl;
+        boost::shared_ptr<D> p = shared_from_this();
+    }    
+};
+
+int main()
+{
+    boost::shared_ptr<D> p(new D);
+    p->func();
+    return 0;    
+}    
